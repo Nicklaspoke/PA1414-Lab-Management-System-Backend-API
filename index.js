0d81d563ee6713ffc1ccc8fb7f5e8e48d66c96fb
Nicklaspoke <Nicklas.poke@gmail.com>
2019-09-21 23:43:41 +0200

    Express server setup and basic test route added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Test, Derpy is best pone');
+/**
+ * Express server for the
+ * Lab Management System API in PA1414
+ *
+ * @author Nicklas König (niko14)
+ */
+'use strict';
+//  Import express, router, middleware for logging and setup settings
+const express = require('express');
+const setup = require('./config/setup.json');
+const apiRouter = require('./route/api.js');
+
+//  Define server variable and port
+const port = setup.port;
+const server = express();
+
+//  Set up router, middleware and start the server
+server.use('/', apiRouter);
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
---
+++ route/api.js
@@ -0,0 +1,23 @@
+/**
+ * Route handler for the api
+ *
+ * @author Nicklas König (niko14)
+ */
+
+'use strict';
+
+//  Import express, bodyparser and database call handler.
+const express = require('express');
+const apiSql = require('../src/apiSql.js');
+const bodyparser = require('body-parser');
+
+//  Define router and urlencodedparser
+const router = express.Router();
+const urlencodedparser = bodyparser.urlencoded({extended: false});
+
+//  Handle the different routes
+router.get('/test', (req, res) => {
+    res.json({message: 'Derpy is best pone'});
+});
+
+module.exports = router;
