6e88873c6a0473244dc2e6babcabd96b042f83a4
Nicklaspoke <Nicklas.poke@gmail.com>
2019-10-08 17:45:54 +0200

    maybe now

--- index.js
+++ index.js
@@ -11,16 +11,19 @@ const setup = require('./config/config.json');
 const apiRouter = require('./route/api.js');
 const path = require('path');
 const cors = require('cors');
+const corsOptions = {
+    origin: '*',
+}
 
 //  Define server variable and port
 const port = setup.port;
 const server = express();
 
 //  Set up and start the server
+server.use(cors(corsOptions));
 server.set('view engine', 'ejs');
 server.use('/', apiRouter);
 server.use(express.static(path.join(__dirname, 'public')));
-server.use(cors());
 server.listen(port, () => {
     console.log(`Server listening at ${port}`);
 });
